f03a57bd6ca296dfb9588ccffffe17af8227d87a
StringSpliter <1436474557@qq.com>
2017-08-02 08:30:38 +0800

    update

--- src/main/java/pri/yhl/datastructure/MaxHeap.java
+++ src/main/java/pri/yhl/datastructure/MaxHeap.java
@@ -37,24 +37,41 @@ public class MaxHeap<T extends Comparable> {
         }
 
     }
+//
+//    public void insert(T ele){
+//        if(length+1>capacity){
+//            ensureCap();
+//        }
+//        elements[length] = ele;
+//        int upIndex = length;
+//        int parentIndex = (length-1)/2;
+//        while(parentIndex>=0 &&  ele.compareTo(elements[parentIndex])>0) {
+//            Object tmp = elements[parentIndex];
+//            elements[parentIndex] = elements[upIndex];
+//            elements[upIndex] = tmp;
+//            upIndex = parentIndex;
+//            parentIndex = (upIndex-1)/2;
+//        }
+//        length = length+1;
+//
+//    }
+
 
     public void insert(T ele){
         if(length+1>capacity){
             ensureCap();
         }
         elements[length] = ele;
-        int upIndex = length;
-        int parentIndex = (length-1)/2;
-        while(parentIndex>=0 &&  ele.compareTo(elements[parentIndex])>0) {
-            Object tmp = elements[parentIndex];
-            elements[parentIndex] = elements[upIndex];
-            elements[upIndex] = tmp;
-            upIndex = parentIndex;
-            parentIndex = (upIndex-1)/2;
+        int i = length;
+        while(i>0 && ele.compareTo(elements[(i-1)/2])>0){
+            elements[i] = elements[(i-1)/2];
+            i = (i-1)/2;
         }
+        elements[i] = ele;
         length = length+1;
 
     }
+
 
     private void ensureCap(){
         elements = Arrays.copyOf(elements,elements.length+increase,Object[].class);
@@ -63,6 +80,7 @@ public class MaxHeap<T extends Comparable> {
     }
 
     public static void main(String args[]){
+        int a = -1/2;
         MaxHeap<Integer> maxHeap = new MaxHeap<Integer>();
         maxHeap.insert(new Integer(10));
         maxHeap.insert(new Integer(30));
